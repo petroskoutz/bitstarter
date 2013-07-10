6e2cce99a908bf5990fd3cdd75e2edb5b54ec542
Ubuntu <ubuntu@ip-172-31-0-235.us-west-2.compute.internal>
2013-07-10 22:31:29 +0000

    new grader2

--- grader.js
+++ grader.js
@@ -75,7 +75,6 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.option('-u, --url <url>', 'Path to index.html', myurl, HTMLFILE_DEFAULT)
         .parse(process.argv);
     if (indic==1){
